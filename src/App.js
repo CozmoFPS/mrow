39d52a390621046d6c577e9f200ad2599ee64821
cozmofps <taingo2005@gmail.com>
2024-11-28 02:13:07 -0800

    hi

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./App.css";
+
+export function uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh() {
+  return (
+    <div>
+      <h1>Hello world :3</h1>
+
+      <h3>The CIA hid listening devices under your skin.</h3> 
+        
+        <br></br>
+
+        <h3>Although you cannot get them out, here are things can do that I learned from CMPR100 to help your privacy:</h3> 
+
+      <ol><b><p>1. Turn of permissions in your device configuration</p></b> 
+
+      <p>All application permissions can be managed under your system settings</p>
+
+      <br></br>
+
+      <b><p>2. Code in Neovim and TOR everything you do because the NSA tries to deanonymize you using your exit nodes</p></b> 
+
+      <p>Because VScode is an agent of microsoft and will spy on everything you work on.... actually you know what, just create your own operating system 
+        like terry davis you'll be 100% safe and protected by god</p></ol> 
+
+        <b><p>here's what neovim is:</p></b>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/c4OyfL5o7DU?si=lfEGHrFejnxLdXQP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        <br></br>
+        <br></br>
+
+      <h3>on the bright side, here's a cute little show from bilibili to take your mind of lizard people trying to control us :3</h3>
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/52SEMKujJ5k?si=KChUbfJr_szxo-l5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    
+    </div>
+  )
+}
+
+
+
+export default uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
