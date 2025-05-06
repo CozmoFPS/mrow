65d9dd89b77e1dc96c845ca4e083eb3271f089a0
CozmoFPS <taingo2005@gmail.com>
2025-05-06 01:48:16 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,10 +11,8 @@ export function uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh() {
       <h3>while you wait for this website to be finished, please watch this wonderful webseries or visit my <a href="https://cozmofps.carrd.co/#" target="_blank">carrd</a>!!&lt;333</h3>
 
       <iframe width="560" height="315" src="https://www.youtube.com/embed/52SEMKujJ5k?si=KChUbfJr_szxo-l5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    
+    <img src="https://photovault.store/P16GFV">
     </div>
-
-  <img src="https://photovault.store/P16GFV">
   )
 }
 
