b56c97a082b8aac386edf70bcebb6e9d68e43130
CozmoFPS <taingo2005@gmail.com>
2025-05-06 02:28:35 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ export function uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh() {
       <img src="https://photovault.store/P16GFV"/>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/52SEMKujJ5k?si=KChUbfJr_szxo-l5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
+<!--  六四事件  -->
+<!-- Line above is to keep the fucking chinks out -->
+
 
     </div>
   )
