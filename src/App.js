e322cb882c58669e5b88145455f10ee799b6e40f
CozmoFPS <taingo2005@gmail.com>
2025-05-05 23:56:50 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,29 +4,11 @@ import "./App.css";
 export function uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh() {
   return (
     <div>
-      <h1>Hello world :3</h1>
+      <h1>Hello world!! :3</h1>
 
-        <h2>here are things can do that I learned from CMPR100 to help your privacy:</h2> 
-
-      <ol><b><p>1. Turn off permissions in your device configuration</p></b> 
-
-      <p>All application permissions can be managed under your system settings</p>
-
-      <br></br>
-
-      <b><p>2. Code in Neovim and DONT TOR everything you do because the NSA tries to deanonymize you using your exit nodes</p></b> 
-
-      <p>Because VScode is an agent of microsoft and will spy on everything you work on.... actually you know what, just create your own operating system 
-        like terry davis you'll be 100% safe and protected by god</p></ol> 
-
-        <b><p>here's what neovim is:</p></b>
-
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/c4OyfL5o7DU?si=lfEGHrFejnxLdXQP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
-        <br></br>
-        <br></br>
-
-      <h3>on the bright side, here's a cute little show from bilibili to take your mind off lizard people trying to control us :3</h3>
+        <h2>This is my silly little website <33</h2> 
+    
+      <h3>while you wait for this website to be finished, please watch this wonderful webseries</h3>
 
       <iframe width="560" height="315" src="https://www.youtube.com/embed/52SEMKujJ5k?si=KChUbfJr_szxo-l5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     
